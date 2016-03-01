03a17570999d90cfd7b96b50e8292ecf1d582e90
Ben Hoffman <bah8892@g.rit.edu>
2016-03-01 10:07:53 -0500

    Started the FileInput class

---
+++ GreatGame/FileInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace GreatGame
+{
+    class FileInput<T>
+    {
+        // Fields
+        private String fileName;
+        private List<T> list;
+
+        public string FileName { get { return this.fileName; } }
+
+        /// <summary>
+        /// This class will have a list of whatever kind of object 
+        /// is passed in with the parameters
+        /// </summary>
+        /// <param name="fileName"></param>
+        public FileInput(String fileName)
+        {
+            this.fileName = fileName;
+            this.list = new List<T>();
+
+        }
+    }
+}
--- GreatGame/Game1.cs
+++ GreatGame/Game1.cs
@@ -25,6 +25,8 @@ namespace GreatGame
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            
+
             Content.RootDirectory = "Content";
         }
 
